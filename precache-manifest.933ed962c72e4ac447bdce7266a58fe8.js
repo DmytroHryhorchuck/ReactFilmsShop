077cedaa2886e85eee2bad9a4ff925ea4913aa6a
Дмитро Григорчук <hryhorchuck_do@local.asv>
2020-04-25 10:16:08 +0300

    Updates

---
+++ precache-manifest.933ed962c72e4ac447bdce7266a58fe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536fe0248bcca7b0838de08fed222ee5",
+    "url": "/reactwarriors-workshop-master/index.html"
+  },
+  {
+    "revision": "6ff8631cadb15131597c",
+    "url": "/reactwarriors-workshop-master/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7821a107663b1eb36d7f",
+    "url": "/reactwarriors-workshop-master/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "6ff8631cadb15131597c",
+    "url": "/reactwarriors-workshop-master/static/js/2.19e12099.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactwarriors-workshop-master/static/js/2.19e12099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7821a107663b1eb36d7f",
+    "url": "/reactwarriors-workshop-master/static/js/main.a4979bf2.chunk.js"
+  },
+  {
+    "revision": "aefaf21efe8d0019860e",
+    "url": "/reactwarriors-workshop-master/static/js/runtime-main.6ed04558.js"
+  }
+]);
